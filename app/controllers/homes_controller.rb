c4d66a28d947dc3d458b1a303da1efe26128aa5c
michaelsimon <michael.simon@gmail.com>
2017-02-25 22:06:34 -0500

    remove commented (unused) lines, correct indentations

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,8 @@
 class HomesController < ApplicationController
-def index
-	if !params[:public_id].blank?
-		redirect_to trip_path(params[:public_id])
+	def index
+		if !params[:public_id].blank?
+			redirect_to trip_path(params[:public_id])
+		end
 	end
-
-end
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,5 @@
 class Section < ActiveRecord::Base
 	has_many :students
-	#accepts_nested_attributes_for :students
 	belongs_to :teacher
 	has_many :trips
 	validates :name, presence: true
