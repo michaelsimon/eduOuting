8cce1cfadd7dd5718eadc4a5cffa0733bcf91ac6
michaelsimon <michael.simon@gmail.com>
2017-02-26 20:58:07 -0500

    DRY up Trips Controller, make params naming and destroy action consistent with other models

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,16 +1,13 @@
 class TripsController < ApplicationController
-  before_action :load_trip, only: [:edit, :update, :show, :destroy]
+  before_action :get_trip, only: [:edit, :update, :show, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
-  
+
   def index
-    @sections = Section.where(teacher_id: current_user.id)
-    @trips = Trip.where(section_id: @sections)
+    @trips = Trip.joins(section: :teacher).where(teachers: {id: current_user.id})
   end
 
   def show
-    @section = Section.find(@trip.section_id)
     @venue =   Trip.fsq_client.venue(@trip.foursquare_locid)
-    @teacher = Teacher.find(@section.teacher_id)
     @photos = Trip.fsq_client.venue_photos(@trip.foursquare_locid, options = {:limit => 6}).items
   end
 
@@ -20,7 +17,7 @@ class TripsController < ApplicationController
   end
 
   def create
-    @trip = Trip.new safe_trip_params
+    @trip = Trip.new(trip_params)
     @trip.public_id = SecureRandom.hex(3).upcase
     if @trip.save
       redirect_to @trip
@@ -35,7 +32,7 @@ class TripsController < ApplicationController
   end
 
   def update
-    if @trip.update safe_trip_params
+    if @trip.update(trip_params)
       redirect_to @trip
     else
       @sections = Section.where(teacher_id: current_user.id)
@@ -44,17 +41,20 @@ class TripsController < ApplicationController
   end
 
   def destroy
-    Trip.find_by(public_id: params[:public_id]).delete
-    redirect_to trips_path
+    if @trip.delete
+      redirect_to trips_path
+    else
+      redirect_to trips_path
+      end
   end
 
   private
-  def safe_trip_params
-    params.require('trip').permit(:name, :section_id, :start_date, :end_date, :cost, :foursquare_locid, :details)
+  def trip_params
+    params.require(:trip).permit(:name, :section_id, :start_date, :end_date, :cost, :foursquare_locid, :details)
   end
 
-  def load_trip
-    @trip = Trip.where(public_id: params[:public_id]).take
+  def get_trip
+    @trip = Trip.find_by(public_id: params[:public_id])
   end
 
 end
