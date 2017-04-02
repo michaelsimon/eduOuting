c755ccec7d674faa35756214f5d044383beccd06
michaelsimon <michael.simon@gmail.com>
2017-04-02 17:18:19 -0400

    create model for StudentSection, update relationship to model for section and Student

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,7 @@
 class Section < ApplicationRecord
-	has_many :students
 	belongs_to :teacher
+	has_many :student_sections
+	has_many :students, through: :student_sections
 	has_many :trips
 	validates :name, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ApplicationRecord
-	belongs_to :section
+	has_many :student_sections
+	has_many :sections, through: :student_sections
 	validates :first_name, :last_name, :section_id, presence: true
 end
