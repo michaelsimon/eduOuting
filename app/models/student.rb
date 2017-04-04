387ff4ee2110f87eb6ec7c049e9f55ce26961d1e
michaelsimon <michael.simon@gmail.com>
2017-04-03 21:41:53 -0400

    remove validation on section_id due to new table

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
 	has_many :student_sections
 	has_many :sections, through: :student_sections
-	validates :first_name, :last_name, :section_id, presence: true
+	validates :first_name, :last_name, presence: true
 end
